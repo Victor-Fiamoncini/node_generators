e48e21992e3fe06eed2931cbd8518bdb11612965
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-01-20 19:31:04 -0300

    feat: add base servers & add axios and uuid as deps

--- src/main/app.js
+++ src/main/app.js
@@ -1,1 +1,43 @@
-console.log('hello')
+import { createServer } from 'http'
+import { parse } from 'url'
+import { v4 as uuid } from 'uuid'
+
+const FIRST_SERVER_PORT = 3000
+const SECOND_SERVER_PORT = 3001
+
+async function firstServerHandler(req, res) {
+	if (req.method === 'GET' && req.url.includes('products')) {
+		const { query } = parse(req.url, true)
+
+		if (query.product_name) {
+			const product = {
+				id: uuid(),
+				name: query.product_name,
+			}
+
+			return res.end(JSON.stringify({ product }))
+		}
+	}
+
+	return res.end('Unable to process request')
+}
+
+async function secondServerHandler(req, res) {
+	if (req.method === 'POST' && req.url.includes('cart')) {
+		for await (const data of req) {
+			const product = JSON.parse(data)
+
+			return res.end(`Process succeeded for ${product?.id}`)
+		}
+	}
+
+	return res.end('Unable to process request')
+}
+
+createServer(firstServerHandler).listen(FIRST_SERVER_PORT, () => {
+	console.log(`First server running at: ${FIRST_SERVER_PORT}`)
+})
+
+createServer(secondServerHandler).listen(SECOND_SERVER_PORT, () => {
+	console.log(`Second server running at: ${SECOND_SERVER_PORT}`)
+})
